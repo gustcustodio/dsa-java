4e29b4da93b77fb56a0670e5697e61793c51a787
gustcustodio <gustcustodio@gmail.com>
2025-06-01 13:15:21 -0300

    feat: aula 02 - vetores e arrays: introdução

---
+++ src/com/loiane/dsa/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.loiane.dsa.vetor;
+
+public class Vetor {
+    private String[] elementos;
+
+    public Vetor(int capacidade) {
+        this.elementos = new String[capacidade];
+    }
+}
---
+++ src/com/loiane/dsa/vetor/teste/Aula02.java
@@ -0,0 +1,12 @@
+package com.loiane.dsa.vetor.teste;
+
+import com.loiane.dsa.vetor.Vetor;
+
+public class Aula02 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(5);
+
+        int i = 0;
+        i++;
+    }
+}
