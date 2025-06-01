237592194471e906a6e0005e1a5a5bd1ac155f2b
gustcustodio <gustcustodio@gmail.com>
2025-06-01 14:11:23 -0300

    feat: aula 04 - vetores e arrays: tamanho e imprimir elementos

--- src/com/loiane/dsa/vetor/Vetor.java
+++ src/com/loiane/dsa/vetor/Vetor.java
@@ -43,4 +43,26 @@ public class Vetor {
         return false;
     }
 
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            sb.append(this.elementos[i]);
+            sb.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            sb.append(this.elementos[this.tamanho - 1]);
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
 }
---
+++ src/com/loiane/dsa/vetor/teste/Aula04.java
@@ -0,0 +1,15 @@
+package com.loiane.dsa.vetor.teste;
+
+import com.loiane.dsa.vetor.Vetor;
+
+public class Aula04 {
+    public static void main(String[] args) {
+        Vetor vetor = new Vetor(10);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+
+        System.out.println(vetor);
+    }
+}
