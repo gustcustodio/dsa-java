7fb6e99ed9f6aaed8203bcff565e049bdc117738
gustcustodio <gustcustodio@gmail.com>
2025-06-05 06:21:03 -0300

    feat: aula 11 - vetores e arrays: configurar tipo dinamicamente

---
+++ src/com/loiane/dsa/vetor/Lista.java
@@ -0,0 +1,107 @@
+package com.loiane.dsa.vetor;
+
+import java.lang.reflect.Array;
+
+public class Lista<T> {
+    private T[] elementos;
+    private int tamanho;
+
+    @SuppressWarnings("unchecked")
+    public Lista(int capacidade) {
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public Lista(int capacidade, Class<T> tipoClasse) {
+        this.elementos = (T[]) Array.newInstance(tipoClasse, capacidade);
+        this.tamanho = 0;
+    }
+
+    public boolean adiciona(T elemento) {
+        this.aumentaCapacidade();
+        if (this.tamanho < this.elementos.length) {
+            this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
+            return true;
+        }
+        return false;
+    }
+
+    public void adiciona(int posicao, T elemento) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        this.aumentaCapacidade();
+
+        for (int i = this.tamanho - 1; i >= posicao; i--) {
+            this.elementos[i + 1] = this.elementos[i];
+        }
+
+        this.elementos[posicao] = elemento;
+        this.tamanho++;
+    }
+
+    public void remove(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+        for (int i = posicao; i < this.tamanho - 1; i++) {
+            this.elementos[i] = this.elementos[i + 1];
+        }
+        this.tamanho--;
+        this.elementos[this.tamanho] = null;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void aumentaCapacidade() {
+        if (this.tamanho == this.elementos.length) {
+            T[] elementosNovos = (T[]) new Object[this.elementos.length * 2];
+            for (int i = 0; i < this.elementos.length; i++) {
+                elementosNovos[i] = this.elementos[i];
+            }
+            this.elementos = elementosNovos;
+        }
+    }
+
+    public T busca(int posicao) {
+        if (!(posicao >= 0 && posicao < tamanho)) {
+            throw new IllegalArgumentException("Posição inválida");
+        }
+
+        return this.elementos[posicao];
+    }
+
+    public int busca(T elemento) {
+        for (int i = 0; i < tamanho; i++) {
+            if (elementos[i].equals(elemento)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int getTamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            sb.append(this.elementos[i]);
+            sb.append(", ");
+        }
+
+        if (this.tamanho > 0) {
+            sb.append(this.elementos[this.tamanho - 1]);
+        }
+
+        sb.append("]");
+
+        return sb.toString();
+    }
+}
---
+++ src/com/loiane/dsa/vetor/teste/Aula11.java
@@ -0,0 +1,13 @@
+package com.loiane.dsa.vetor.teste;
+
+import com.loiane.dsa.vetor.Lista;
+
+public class Aula11 {
+    public static void main(String[] args) {
+        Lista<Integer> vetor = new Lista<>(1);
+
+        vetor.adiciona(1);
+
+        System.out.println(vetor);
+    }
+}
