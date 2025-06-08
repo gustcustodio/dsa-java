aba8537aca36667bca8d3949ec069d45b20a97e5
gustcustodio <gustcustodio@gmail.com>
2025-06-08 12:23:31 -0300

    feat: aula 13 - pilhas: introdução

---
+++ src/com/loiane/dsa/pilha/Pilha.java
@@ -0,0 +1,13 @@
+package com.loiane.dsa.pilha;
+
+import com.loiane.dsa.base.EstruturaEstatica;
+
+public class Pilha<T> extends EstruturaEstatica<T> {
+    public Pilha() {
+        super();
+    }
+
+    public Pilha(int capacidade) {
+        super(capacidade);
+    }
+}
